22caec2e1f87d978e77f3bf701d4e67e4dbe72a2
Brandon Carpenter <brandon.carpenter@pnnl.gov>
2014-05-09 09:25:05 -0700

    Add auto version to wheel.

--- setup.py
+++ setup.py
@@ -1,8 +1,40 @@
+import os
+import sys
+
 from setuptools import setup
+
+
+__version__ = '0.0'
+
+
+def make_version():
+    build_num = os.environ.get('BUILD_NUMBER', '')
+    hash_num = ''
+    try:
+        file = open('.git/HEAD')
+    except FileNotFoundError:
+        pass
+    else:
+        with file:
+            for line in file:
+                if line.startswith('ref:'):
+                    ref = line[4:].strip()
+                    try:
+                        hash_num = open('.git/' + ref).read()
+                    except FileNotFoundError:
+                        pass
+                    break
+    version = __version__
+    if build_num:
+        version += 'dev' + build_num
+    if hash_num:
+        version += '+' + hash_num[:10]
+    return version
+
 
 setup(
     name = 'openeis-ui',
-    version = '0.0dev0',
+    version = make_version(),
     description = 'Open Energy Information System (OpenEIS) API client.',
     author = 'Bora Akyol',
     author_email = 'bora@pnnl.gov',
