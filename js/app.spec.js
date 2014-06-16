b0cbd2442da4df315b3cf2010a641b8028406959
Tim Kang <tim.kang@pnnl.gov>
2014-06-16 10:45:14 -0700

    Add test for 'capitalize' filter

--- js/app.spec.js
+++ js/app.spec.js
@@ -2,4 +2,16 @@ describe('openeis-ui', function () {
     beforeEach(function () {
         module('openeis-ui');
     });
+
+    describe('capitalize filter', function () {
+        it('should capitalize the first letter of strings', inject(function (capitalizeFilter) {
+            expect(capitalizeFilter('word')).toEqual('Word');
+            expect(capitalizeFilter('multiple words')).toEqual('Multiple words');
+            expect(capitalizeFilter('Already capitalized')).toEqual('Already capitalized');
+            expect(capitalizeFilter('second Word')).toEqual('Second Word');
+            expect(capitalizeFilter('123abc')).toEqual('123abc');
+            expect(capitalizeFilter('')).toEqual('');
+            expect(capitalizeFilter(null)).toEqual('');
+        }));
+    });
 });
