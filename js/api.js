e8d57f150bab69648c95fb08bf4621978805bc85
Nathan Tenney <nathan.tenney@pnnl.gov>
2014-07-10 13:51:30 -0700

    Added create method to DataReports

--- js/api.js
+++ js/api.js
@@ -307,8 +307,12 @@ angular.module('openeis-ui.api', ['ngResource'])
 })
 .service('DataReports', function(){
     var DataReports = this;
+    var ReportStatus = [{"name": "p1", "status": "Queued"}, {"name":"p2","status": "Running"}, {"name":"p3","status":"Completed"}];
     DataReports.query = function () {
-        return [{"name": "p1", "status": "Queued"}, {"name":"p2","status": "Running"}, {"name":"p3","status":"Completed"}];
-    };    
+        return ReportStatus;
+    };
+    DataReports.create = function(status){
+        ReportStatus.push(status);
+    }
 });
 
