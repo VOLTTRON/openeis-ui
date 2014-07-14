d3ec071f4312e4af11f0a7bc85863c1961d30097
Tim Kang <tim.kang@pnnl.gov>
2014-07-14 14:04:41 -0700

    Move NewDataSetCtrl controller into own module

--- js/project.js
+++ js/project.js
@@ -3,32 +3,7 @@ angular.module('openeis-ui.project', [
     'openeis-ui.filters',
     'openeis-ui.modals',
     'openeis-ui.projects.data-maps',
-    'openeis-ui.projects.data-sets',
 ])
-.controller('NewDataSetCtrl', function ($scope, DataSets, DataMaps, Modals) {
-    DataMaps.ensureFileMetaData($scope.dataFiles);
-
-    $scope.newDataSet = { files: {} };
-
-    $scope.save = function () {
-        var files = [];
-
-        angular.forEach($scope.newDataSet.files, function (fileId, key) {
-            files.push({ name: key, file: fileId });
-        });
-
-        DataSets.create({
-            map: $scope.newDataSet.map.id,
-            files: files,
-        }).$promise.then(function (dataSet) {
-            $scope.dataSets.push(dataSet);
-            $scope.statusCheck();
-            Modals.closeModal('newDataSet');
-        }, function (rejection) {
-            alert(angular.toJson(rejection, true));
-        });
-    };
-})
 .controller('NewDataReportCtrl', function ($scope, Applications, DataMaps, $q, Modals) {
     $scope.newDataReport = {};
 
