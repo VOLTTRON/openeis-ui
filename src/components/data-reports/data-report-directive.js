9267b246cbc4aeeed12b3d98981853aea82ba1f1
Tim Kang <tim.kang@pnnl.gov>
2014-07-23 15:44:25 -0700

    Initial implementation of line plot

--- src/components/data-reports/data-report-directive.js
+++ src/components/data-reports/data-report-directive.js
@@ -83,7 +83,56 @@ angular.module('openeis-ui.data-reports.app-report-directive', [])
     }
 
     function linePlotSVG(data, xLabel, yLabel) {
-        // TODO: create (with D3.js) and return SVG
+        // Adapted from http://bl.ocks.org/mbostock/3883245
+
+        var margin = {top: 20, right: 20, bottom: 30, left: 50},
+            width = 920 - margin.left - margin.right,
+            height = 300 - margin.top - margin.bottom;
+
+        var x = d3.scale.linear().range([0, width]);
+
+        var y = d3.scale.linear().range([height, 0]);
+
+        var xAxis = d3.svg.axis().scale(x).orient('bottom');
+
+        var yAxis = d3.svg.axis().scale(y).orient('left');
+
+        var line = d3.svg.line()
+            .x(function(d) { return x(d.x); })
+            .y(function(d) { return y(d.y); })
+            .interpolate('basis');
+
+        var svg = d3.select(document.createElementNS('http://www.w3.org/2000/svg', 'svg'))
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom);
+
+        var graph = svg.append('g')
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+        x.domain(d3.extent(data, function(d) { return d.x; }));
+        y.domain(d3.extent(data, function(d) { return d.y; }));
+
+        graph.append('g')
+            .attr('class', 'line-plot__axis line-plot__axis--x')
+            .attr('transform', 'translate(0,' + height + ')')
+            .call(xAxis);
+
+        graph.append('g')
+            .attr('class', 'line-plot__axis line-plot__axis--y')
+            .call(yAxis)
+            .append('text')
+                .attr('transform', 'rotate(-90)')
+                .attr('y', 6)
+                .attr('dy', '.71em')
+                .style('text-anchor', 'end')
+                .text(yLabel);
+
+        graph.append('path')
+            .datum(data)
+            .attr('class', 'line-plot__line')
+            .attr('d', line);
+
+        return svg[0];
     }
 
     function barChartSVG(data, xLabel, yLabel) {
