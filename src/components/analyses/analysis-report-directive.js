05fe20dc6503e13988a746eeaf9c7e4ab7dfd5cf
Jereme Haack <Jereme.Haack@pnnl.gov>
2014-08-04 16:19:28 -0700

    Added initial ScatterPlot implementation.

--- src/components/analyses/analysis-report-directive.js
+++ src/components/analyses/analysis-report-directive.js
@@ -117,7 +117,125 @@ angular.module('openeis-ui.analyses.analysis-report-directive', [])
     }
 
     function scatterPlotSVG(data, xLabel, yLabel) {
-        // TODO: create (with D3.js) and return SVG
+        var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 920 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+        /*
+         * value accessor - returns the value to encode for a given data object.
+         * scale - maps value to a visual display encoding, such as a pixel position.
+         * map function - maps from data value to display value
+         * axis - sets up axis
+         */
+
+        // setup x
+        var xValue = function(d) { return d.x;}, // data -> value
+            xScale = d3.scale.linear().range([0, width]), // value -> display
+            xMap = function(d) { return xScale(xValue(d));}, // data -> display
+            xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+        // setup y
+        var yValue = function(d) { return d.y;}, // data -> value
+            yScale = d3.scale.linear().range([height, 0]), // value -> display
+            yMap = function(d) { return yScale(yValue(d));}, // data -> display
+            yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+        // setup fill color
+        var cValue = function(d) { return 0;},
+            color = d3.scale.category10();
+
+        // add the graph canvas to the body of the webpage
+        var svg = d3.select(document.createElementNS('http://www.w3.org/2000/svg', 'svg'))
+                    .attr('width', width + margin.left + margin.right)
+                    .attr('height', height + margin.top + margin.bottom);
+
+
+        var graph = svg.append('g')
+                    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        //replace svg with graph
+
+        // add the tooltip area to the webpage
+        var tooltip = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
+
+          // don't want dots overlapping axis, so add in buffer to data domain
+          xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
+          yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+
+          // x-axis
+          graph.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis)
+            .append("text")
+              .attr("class", "label")
+              .attr("x", width)
+              .attr("y", -6)
+              .style("text-anchor", "end")
+              .text(xLabel);
+
+          // y-axis
+          graph.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+            .append("text")
+              .attr("class", "label")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text(yLabel);
+
+          // draw dots
+          graph.selectAll(".dot")
+              .data(data)
+            .enter().append("circle")
+              .attr("class", "dot")
+              .attr("r", 3.5)
+              .attr("cx", xMap)
+              .attr("cy", yMap)
+              .style("fill", function(d) { return color(cValue(d));})
+//              .on("mouseover", function(d) {
+//                 tooltip.transition()
+//                       .duration(200)
+//                       .style("opacity", .9);
+//                  tooltip.html("data point" + "<br/> (" + xValue(d)
+//                    + ", " + yValue(d) + ")")
+//                       .style("left", (d3.event.pageX + 5) + "px")
+//                       .style("top", (d3.event.pageY - 28) + "px");
+//              })
+//              .on("mouseout", function(d) {
+//                  tooltip.transition()
+//                       .duration(500)
+//                       .style("opacity", 0);
+//              });
+
+/*
+          // draw legend
+          var legend = graph.selectAll(".legend")
+              .data(color.domain())
+            .enter().append("g")
+              .attr("class", "legend")
+              .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+          // draw legend colored rectangles
+          legend.append("rect")
+              .attr("x", width - 18)
+              .attr("width", 18)
+              .attr("height", 18)
+              .style("fill", color);
+
+          // draw legend text
+          legend.append("text")
+              .attr("x", width - 24)
+              .attr("y", 9)
+              .attr("dy", ".35em")
+              .style("text-anchor", "end")
+              .text(function(d) { return d;})
+*/
+            return svg[0];
     }
 
     function heatMapSVG(data, xLabel, yLabel) {
