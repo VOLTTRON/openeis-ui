2a206425717dc400cd23c51f0caca480669a3070
Poorva Sharma <poorva.sharma@pnnl.gov>
2014-08-07 11:33:07 -0700

    added heat map

--- src/components/analyses/analysis-report-directive.js
+++ src/components/analyses/analysis-report-directive.js
@@ -38,7 +38,7 @@ angular.module('openeis-ui.analyses.analysis-report-directive', [])
                     element.append(angular.element('<div class="scatter-plot" />').append(scatterPlotSVG(getXYDataSet(), reportElement.x_label, reportElement.y_label)));
                     break;
                 case 'HeatMap':
-                    //element.append(angular.element('<div class="heat-map" />').append(heatMapSVG(getXYZDataSet(), reportElement.x_label, reportElement.y_label)));
+                    element.append(angular.element('<div class="heat-map" />').append(heatMapSVG(getXYZDataSet(), reportElement.x_label, reportElement.y_label)));
                     break;
                 }
             });
@@ -52,7 +52,7 @@ angular.module('openeis-ui.analyses.analysis-report-directive', [])
         for (i = 0; i < 100; i++) {
             data.push({
                 x: i,
-                y: i % (100 / i),
+                y: i % (100 / i)
             });
         }
 
@@ -64,10 +64,27 @@ angular.module('openeis-ui.analyses.analysis-report-directive', [])
         var i, data = [];
 
         for (i = 0; i < 12; i++) {
-            data.push({
+        	data.push({
                 x: i,
-                y: i % (12 / i),
+                y: i % (12 / i)
             });
+        }
+
+        return data;
+    }
+    
+    function getXYZDataSet() {
+        var i, j, data = [];
+
+        for (i = 1; i <= 24; i++) {
+        	for (j = 1; j <= 7; j++) {
+	            data.push({
+	                x: i,
+	                y: j,
+	                value: Math.floor(Math.random() * 24) 
+	            });
+        	}
+            
         }
 
         return data;
@@ -127,7 +144,7 @@ angular.module('openeis-ui.analyses.analysis-report-directive', [])
     }
 
     function barChartSVG(data, xLabel, yLabel) {
-        // TODO: create (with D3.js) and return SVG
+        // Adapte from http://bl.ocks.org/mbostock/3885304
     
         var margin = {top: 20, right: 20, bottom: 30, left: 50},
         	width = 920 - margin.left - margin.right,
@@ -306,6 +323,86 @@ angular.module('openeis-ui.analyses.analysis-report-directive', [])
     }
 
     function heatMapSVG(data, xLabel, yLabel) {
-        // TODO: create (with D3.js) and return SVG
+    	// Adapted from http://bl.ocks.org/tjdecke/5558084
+    	var margin = { top: 50, right: 0, bottom: 100, left: 30 },
+        	width = 960 - margin.left - margin.right,
+        	height = 430 - margin.top - margin.bottom,
+        	gridSize = Math.floor(width / 24),
+        	legendElementWidth = gridSize*2,
+        	buckets = 9,
+        	colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
+        	y_array = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+        	x_array = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
+
+
+    	var colorScale = d3.scale.quantile()
+            .domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
+            .range(colors);
+
+        var svg = d3.select(document.createElementNS('http://www.w3.org/2000/svg', 'svg'))
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom);
+
+        var graph = svg.append('g')
+		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        
+        
+        var yLabels = graph.selectAll(".yLabel")
+            .data(y_array)
+            .enter().append("text")
+              .text(function (d) { return d; })
+              .attr("x", 0)
+              .attr("y", function (d, i) { return i * gridSize; })
+              .style("text-anchor", "end")
+              .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
+              .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "yLabel mono axis axis-workweek" : "yLabel mono axis"); });
+
+        var xLabels = graph.selectAll(".xLabel")
+            .data(x_array)
+            .enter().append("text")
+              .text(function(d) { return d; })
+              .attr("x", function(d, i) { return i * gridSize; })
+              .attr("y", 0)
+              .style("text-anchor", "middle")
+              .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+              .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "xLabel mono axis axis-worktime" : "xLabel mono axis"); });
+
+        var heatMap = graph.selectAll(".value")
+            .data(data)
+            .enter().append("rect")
+            .attr("x", function(d) { return (d.x - 1) * gridSize; })
+            .attr("y", function(d) { return (d.y - 1) * gridSize; })
+            .attr("rx", 4)
+            .attr("ry", 4)
+            .attr("class", "value bordered")
+            .attr("width", gridSize)
+            .attr("height", gridSize)
+            .style("fill", colors[0]);
+
+        heatMap.transition().duration(1000)
+            .style("fill", function(d) { return colorScale(d.value); });
+
+        heatMap.append("title").text(function(d) { return d.value; });
+            
+        var legend = graph.selectAll(".legend")
+            .data([0].concat(colorScale.quantiles()), function(d) { return d; })
+            .enter().append("g")
+            .attr("class", "legend");
+
+        legend.append("rect")
+          .attr("x", function(d, i) { return legendElementWidth * i; })
+          .attr("y", height)
+          .attr("width", legendElementWidth)
+          .attr("height", gridSize / 2)
+          .style("fill", function(d, i) { return colors[i]; });
+
+        legend.append("text")
+          .attr("class", "mono")
+          .text(function(d) { return "≥ " + Math.round(d); })
+          .attr("x", function(d, i) { return legendElementWidth * i; })
+          .attr("y", height + gridSize);
+    
+        return svg[0];
+    	
     }
 });
